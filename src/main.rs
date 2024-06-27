b48d203add433161fabccaa439cfed655872c621
Andy Gayton <andy@thecablelounge.com>
2024-06-27 00:41:27 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-/Users/andy/.s/sessions/03BZK7OIDZKGG7HYH71O9UGN4
+use clap::Parser;
+use webpage::{Webpage, WebpageOptions};
+
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Args {
+    #[clap(value_parser)]
+    url: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let info =
+        Webpage::from_url(&args.url, WebpageOptions::default()).expect("Could not read from URL");
+    println!("{}", serde_json::to_string(&info).unwrap());
+}
